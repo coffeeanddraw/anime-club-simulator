7e8a1914120baa19bfe6690ee11ef9ddba6eb52c
Cattatonicat <threeeyedvenelope@gmail.com>
2019-12-08 19:21:07 -0600

    Add main menu base functionality

---
+++ AnimeClubSimulator/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName;
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
