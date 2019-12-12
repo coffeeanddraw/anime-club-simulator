7f87aac94a55cbc8771c2544ec343ef0d5a35c6f
Cattatonicat <threeeyedvenelope@gmail.com>
2019-12-12 17:45:53 -0600

    Add instructions and about screen

--- AnimeClubSimulator/Assets/Scripts/MainMenu.cs
+++ AnimeClubSimulator/Assets/Scripts/MainMenu.cs
@@ -6,11 +6,37 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]
-    private string sceneName;
+    private string sceneName = "";
+
+    [SerializeField]
+    private GameObject instructionsPanel = null;
+
+    [SerializeField]
+    private GameObject aboutPanel = null;
 
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void OpenInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void CloseInstructions()
+    {
+        instructionsPanel.SetActive(false);
+    }
+
+    public void OpenAbout()
+    {
+        aboutPanel.SetActive(true);
+    }
+
+    public void CloseAbout()
+    {
+        aboutPanel.SetActive(false);
     }
 
     public void ExitGame()
